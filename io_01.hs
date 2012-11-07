f803ea7232791224ee67d3e474bb866292bc54ef
fvdb <frank@am-knie.de>
2012-11-07 10:09:51 +0100

    Add *.bin to .gitignore

---
+++ io_01.hs
@@ -0,0 +1,14 @@
+-- this is a stupid commentar
+-- oh here it goes on
+
+
+main = do
+       putStrLn "Greetings! What is your favorite colour? "
+       inpStr <- getLine
+       putStrLn ("Hey, your favorite colour is " ++ inpStr ++ "!")
+
+       -- with the $ we can save the parenthese
+       putStrLn $ "Hey, your favorite colour is " ++ inpStr ++ "!"
+
+
+
